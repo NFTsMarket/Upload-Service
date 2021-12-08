4a0b26f7a8c3a5fcc10c45896e863dcfee1da29b
reyblacua <reyblacua@alum.us.es>
2021-12-08 13:41:31 +0100

    Dockerización de node y mongo
    Se han creado los archivos Dockerfile y docker-compose y se ha modificado db.js para permitir el desarrollo en local a parte de la dockerización. Issue #29

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
-const DB_URL = (process.env.MONGO_URL || 'mongodb://localhost:27017/test');
+
+var DB_URL = ('mongodb://localhost:27017/test');
+
+if(process.env.MONGO_HOSTNAME!=undefined){
+    DB_URL = (process.env.MONGO_URL || 'mongodb://'+process.env.MONGO_HOSTNAME+':27017/test');
+}
 
 const dbConnect = function() {
+    console.log(DB_URL)
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
     return mongoose.connect(DB_URL, { useNewUrlParser: true });
