dda7c883c7928b5a934284bd47395a497b75d8aa
reyblacua <reyblacua@alum.us.es>
2021-12-29 13:57:58 +0100

    feat: Integración de google photos
    Se ha añadido la integración en el list, get y create de asset. Update y delete no han sido implementados ya que la api de google photos no lo permite. Issue #34

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const googlePhotos= require('./googlePhotos/googlePhotosService')
 
 var DB_URL = ('mongodb://localhost:27017/test');
 
@@ -9,6 +10,7 @@ if(process.env.MONGO_HOSTNAME!=undefined){
 const dbConnect = function() {
     console.log(DB_URL)
     const db = mongoose.connection;
+    googlePhotos.initializeGoogleCloud();
     db.on('error', console.error.bind(console, 'connection error: '));
     return mongoose.connect(DB_URL, { useNewUrlParser: true });
 }
--- models/asset.js
+++ models/asset.js
@@ -6,7 +6,7 @@ const schema = new Schema({
   file: {
     type:String,
     required:[true, "The file can not be empty"],
-    match: [/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/, 'The URL is not valid']
+    // match: [/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/, 'The URL is not valid']
   },
   name: {
     type:String,
