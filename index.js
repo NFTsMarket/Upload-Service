3f44044fdee210f3ab1adbb59a1cab16ec473ad6
juaferfer11 <juaferfer11@alum.us.es>
2021-12-29 18:29:13 +0100

    fix: Se intenta imprimir una variable de entorno de okteto
    Issue #40

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./server.js');
 const dbConnect = require('./db');
 const dotenv = require('dotenv');
 dotenv.config();
+console.log(process.env.uploadMongoUrl);
 var port = (process.env.PORT || 8000);
 
 console.log("Starting API server at "+port);
